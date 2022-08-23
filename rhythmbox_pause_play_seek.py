fef4524bd349275d0d38f8212c188e45d964e6a8
Dylan Lobo <dylan.j.lobo@gmail.com>
2022-08-23 14:53:25 +0100

    Corrected comment

--- rhythmbox_pause_play_seek.py
+++ rhythmbox_pause_play_seek.py
@@ -5,7 +5,7 @@ bus = dbus.SessionBus()
 proxy = bus.get_object("org.mpris.MediaPlayer2.rhythmbox", "/org/mpris/MediaPlayer2")
 player = dbus.Interface(proxy, dbus_interface="org.mpris.MediaPlayer2.Player")
 player.Pause()
-# Set to the the start of playing media by specifying a large time value
+# Set to the the start of playing media by specifying a large -ve microsecond value
 player.Seek(-1 * (helpers.to_microsecs("99:59:59")))
 # Seek to the specified time offset
 player.Seek(helpers.to_microsecs("00:05:00"))
