07ce9a7186dcb8f732c069cb5ffd58aeefbfa6f0
Dylan Lobo <dylan.j.lobo@gmail.com>
2022-08-24 00:50:06 +0100

    Added new function to_HHMMSS()

--- helpers.py
+++ helpers.py
@@ -29,3 +29,28 @@ def to_microsecs(time_str: str) -> int:
     mins_in_microsecs = total_mins * 60000000
     total_microsecs = mins_in_microsecs + (secs * 1000000)
     return total_microsecs
+
+
+def to_HHMMSS(microsecs: int) -> str:
+    """Converts time specifed in microseconds to HH:MM:SS time format.
+    The maximum input value is 359999000000, which is equvalent to
+    99:59:59 in HH:MM:SS format.
+
+    Arguments
+    microsecs is an integer, -ve input values are converted to +ve values
+    Returns a string in HH:MM:SS time format.
+    """
+
+    abs_microsecs = abs(microsecs)
+    if abs_microsecs > 359999000000:
+        raise ValueError("Max absolute value of microsecs is 359999000000")
+    total_secs = int(abs_microsecs / 1000000)
+    total_minutes = int(total_secs / 60)
+    left_over_secs = total_secs % 60
+    hours = int(total_minutes / 60)
+    minutes = int(total_minutes % 60)
+    seconds = left_over_secs
+    sec_s = f"{seconds}" if seconds > 9 else f"0{seconds}"
+    min_s = f"{minutes}" if minutes > 9 else f"0{minutes}"
+    hr_s = f"{hours}" if hours > 9 else f"0{hours}"
+    return f"{hr_s}:{min_s}:{sec_s}"
